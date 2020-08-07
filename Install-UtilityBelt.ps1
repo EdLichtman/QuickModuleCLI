bc187bc14033b1b17b220382037c6112dc1b97a7
Eddie Lichtman <e.lichtman@gmail.com>
2020-08-07 06:55:21 -0500

    Added ability to choose Utility to install

--- Install-UtilityBelt.ps1
+++ Install-UtilityBelt.ps1
@@ -1,17 +1,60 @@
+param(
+    [Switch] $InstallEntireBelt,
+    [Switch] $Force
+)
+
 $localHelpersPath = ".\Required"
 . $localHelpersPath\Get-QuickEnvironment.ps1
+. $localHelpersPath\New-FileWithContent.ps1
+
+. .\Installer\Show-Menu.ps1
 
 $QuickForceText = if ($Force) { '-force' } else { '' }
 
 $localUtilityBeltPath = ".\UtilityBelt"
-$localUtilityBeltFunctionsPath = "$localUtilityBeltPath\Functions"
-$utilityBeltFunctions = @(Get-ChildItem $localUtilityBeltFunctionsPath -Filter '*.ps1')
-foreach($function in $utilityBeltFunctions) {
-    Invoke-Expression "New-FileWithContent -FilePath $QuickFunctionsRoot\$function -FileText (Get-Content $localUtilityBeltFunctionsPath\$function -Raw) $QuickForceText"
-}
 
+
+$localUtilityBeltFunctionsPath = "$localUtilityBeltPath\Functions"
+$utilityBeltFunctions = @(Get-ChildItem $localUtilityBeltFunctionsPath -Filter '*.ps1') 
 $localUtilityBeltAliasesPath = "$localUtilityBeltPath\Aliases"
 $utilityBeltAliases = @(Get-ChildItem $localUtilityBeltAliasesPath -Filter '*.ps1')
-foreach($alias in $utilityBeltAliases) {
-    Invoke-Expression "New-FileWithContent -FilePath $QuickAliasesRoot\$alias -FileText (Get-Content $localUtilityBeltAliasesPath\$alias -Raw) $QuickForceText"
+
+$UtilityBeltCommands = New-Object System.Collections.ArrayList
+$utilityBeltFunctions | ForEach-Object {
+    $obj = [PSCustomObject]@{
+        Name = $_ -replace '.ps1', ''
+        InstallLocation = "$QuickFunctionsRoot\$_"
+        InstallerLocation = "$localUtilityBeltFunctionsPath\$_"
+    }
+    $UtilityBeltCommands.Add($obj) | Out-Null;
+}
+$utilityBeltAliases | ForEach-Object {
+    $obj = [PSCustomObject]@{
+        Name = $_ -replace '.ps1', ''
+        InstallLocation = "$QuickAliasesRoot\$_"
+        InstallerLocation = "$localUtilityBeltAliasesPath\$_"
+    }
+    $UtilityBeltCommands.Add($obj) | Out-Null;
+}
+
+if ($InstallEntireBelt) {
+    foreach($tool in $UtilityBeltCommands) {
+        $InstallLocation = $tool.InstallLocation
+        $InstallerLocation = $tool.InstallerLocation
+        Invoke-Expression "New-FileWithContent -FilePath $InstallLocation -FileText (Get-Content $InstallerLocation -Raw) $QuickForceText"
+    }
+} else {
+    $MenuOptions = @{}
+    $MenuOptionMappings = @{}
+    $UtilityBeltCommands | ForEach-Object {
+        $MenuOptions[$_.Name] = $_.Name
+        $MenuOptionMappings[$_.Name] = $_
+    }
+    $ChosenOption = Show-Menu "Install which tool?" $MenuOptions
+
+    $tool = $MenuOptionMappings[$ChosenOption]
+    $InstallLocation = $tool.InstallLocation
+    $InstallerLocation = $tool.InstallerLocation
+    Invoke-Expression "New-FileWithContent -FilePath $InstallLocation -FileText (Get-Content $InstallerLocation -Raw) $QuickForceText"
+    
 }
--- RunMe.ps1
+++ RunMe.ps1
@@ -1,11 +1,12 @@
 param(
     [Switch]$InstallUtilityBelt,
+    [Switch]$InstallUtility,
     [Switch]$Install,
     [Switch]$Uninstall,
     [Switch]$Reinstall,
     [Switch]$PreserveUserDefinedCommands
     )
-if (!$Install -and !$Uninstall -and !$Reinstall -and !$InstallUtilityBelt) {
+if (!$Install -and !$Uninstall -and !$Reinstall -and !$InstallUtilityBelt -and !$InstallUtility) {
     Get-Content .\Readme.md -Raw
 }
 
@@ -19,6 +20,7 @@ if ($Install -or $Reinstall) {
     Invoke-Expression ".\Install-Script.ps1 $QuickForceText"
 }
 
-if ($InstallUtilityBelt) {
-    . ".\Install-UtilityBelt.ps1"
+if ($InstallUtilityBelt -or $InstallUtility) {
+    $InstallEntireBelt = if ($InstallUtilityBelt) { '-InstallEntireBelt' } else { '' }
+    Invoke-Expression ".\Install-UtilityBelt.ps1 $InstallEntireBelt $QuickForceText"
 }
